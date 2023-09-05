727dd183d000885a63a1e0cbb97aca6b2f8b83e5
vahid torkaman <vahidt@spotify.com>
2023-09-05 14:16:35 -0400

    add go demo app to fetch the flags and print it in the terminal

--- pkg/provider/demo/GoDemoApp.go
+++ pkg/provider/demo/GoDemoApp.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/open-feature/go-sdk/pkg/openfeature"
 	confidence "github.com/spotify/confidence-openfeature-provider-go/pkg/provider"
 )
 
 func main() {
 	clientSecret := "CLIENT_SECRET"
-	fmt.Println("Fetching the hawk flag...")
+	fmt.Println("Fetching the flags...")
 
 	provider, err := confidence.NewFlagProvider(confidence.APIConfig{APIKey: clientSecret, Region: confidence.APIRegionEU})
 
@@ -21,22 +22,28 @@ func main() {
 	client := openfeature.NewClient("testApp")
 
 	attributes := make(map[string]interface{})
-	attributes["targeting_key"] = "21339198230981240971409127491"
+	targetingKey := uuid.New().String()
+	attributes["targeting_key"] = targetingKey
 
-	colorValue, _ := client.StringValue(context.Background(), "hawkflag.color", "defaultValue",
-		openfeature.NewEvaluationContext("", attributes))
+	fmt.Println(" Random UUID -> " + targetingKey)
 
-	messageValue, _ := client.StringValue(context.Background(), "hawkflag.message", "defaultValue",
-		openfeature.NewEvaluationContext("", attributes))
+	of := openfeature.NewEvaluationContext("", attributes)
+
+	colorValue, _ := client.StringValue(context.Background(), "hawkflag.color", "defaultValue", of)
+	messageValue, _ := client.StringValue(context.Background(), "hawkflag.message", "defaultValue", of)
 
 	colorYellow := "\033[33m"
+	colorGreen := "\033[32m"
 	colorRed := "\033[31m"
 
-	fmt.Println("Color --> " + colorValue)
+	fmt.Println(" Color --> " + colorValue)
 
-	if colorValue == "Yellow" {
-		fmt.Println("Message -->"+colorYellow, messageValue)
-	} else {
-		fmt.Println("Message -->"+colorRed, messageValue)
+	switch {
+	case colorValue == "Yellow":
+		fmt.Println(colorYellow, "Message --> "+messageValue)
+	case colorValue == "Green":
+		fmt.Println(colorGreen, "Message --> "+messageValue)
+	default:
+		fmt.Println(colorRed, "Message --> "+messageValue)
 	}
 }
--- pkg/provider/go.mod
+++ pkg/provider/go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
