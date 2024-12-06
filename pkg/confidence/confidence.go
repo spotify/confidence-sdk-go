3891d57561b91685155dcd0aed08f10e809e6843
Nicklas Lundin <nicklasl@spotify.com>
2024-12-06 16:36:58 +0100

    fixup! feat: add support for logging

--- pkg/confidence/confidence.go
+++ pkg/confidence/confidence.go
@@ -2,9 +2,11 @@ package confidence
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"net/url"
 	"reflect"
 	"strings"
 	"sync"
@@ -212,6 +214,13 @@ func (e Confidence) ResolveFlag(ctx context.Context, flag string, defaultValue i
 	if err != nil {
 		slog.Warn("Error in resolving flag", "flag", flag, "error", err)
 		return processResolveError(err, defaultValue)
+	}
+	key := url.QueryEscape(e.Config.APIKey)
+	flagEncoded := url.QueryEscape(flag)
+	json, err := json.Marshal(e.GetContext())
+	if err == nil {
+		jsonContextEncoded := url.QueryEscape(string(json))
+		e.Logger.Debug("See resolves for " + flag + " in Confidence: https://app.confidence.spotify.com/flags/resolver-test?client-key=" + key + "&flag=flags/" + flagEncoded + "&context=" + jsonContextEncoded)
 	}
 	if len(resp.ResolvedFlags) == 0 {
 		slog.Debug("Flag not found", "flag", flag)
